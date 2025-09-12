5d189eabbbf29380735f98ba3f0034e0ef7ab818
Oyin <eerinlakatabu@gmail.com>
2025-09-12 18:34:20 -0500

    Add files via upload

---
+++ average.py
@@ -0,0 +1,23 @@
+#There are five trees in Jack's front yard. He checks each tree to find out how tall it is in inches and writes the height on a sheet of paper. Jack's list: 98, 94, 41, 96, and 11. What is the average height of a tree in Jack's front yard?
+
+
+#storing values
+
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 96
+tree5 = 11
+
+#finding the total of trees
+
+sum = tree1+tree2+tree3+tree4+tree5
+
+print("the sum of all the 5 trees is:", sum)
+
+#finding the average of trees
+
+average = sum/5
+
+print("The average of all the trees is:", average )
+
