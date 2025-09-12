d06c2a6a5ee6d7822dadf3f611bf2ad020441a17
Oyin <eerinlakatabu@gmail.com>
2025-09-12 18:44:34 -0500

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,12 @@
+book1 = 45
+book2 = 20
+book3 = 100
+book4 = 31
+book5 = 67
+sum = book1+book2+book3+book4+book5
+
+print("the sum of all the 5 books is:", sum)
+
+average = sum/5
+
+print("The average of all the books is:", average )
